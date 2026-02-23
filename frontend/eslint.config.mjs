00c34025feed86cefeb83d3c683ee6023e85fc24
Shafik Hakim Bin Abdul Wahid <E1582515@u.nus.edu>
2026-02-23 09:14:49 +0800

    Fix ESLint flat config imports for Next preset modules

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -1,4 +1,4 @@
-import nextVitals from 'eslint-config-next/core-web-vitals';
-import nextTypescript from 'eslint-config-next/typescript';
+import nextVitals from 'eslint-config-next/core-web-vitals.js';
+import nextTypescript from 'eslint-config-next/typescript.js';
 
 export default [...nextVitals, ...nextTypescript];
